d8d46ac93c70f88395ea4f0b904996887f28ec9f
ktichmann <ktichmann@gmail.com>
2019-10-10 13:04:56 +0200

    dreamshare! :D

--- stories/AppBar.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/vue'
-import AppBar from '../components/AppBar'
-
-storiesOf('AppBar', module).add('with vuetify', () => ({
-  components: { AppBar },
-  template: '<AppBar />'
-}))
